6ea1f042ca84902dd7357077f45151f4ce8f7918
趙子榮 <alan@25sprout.com>
2021-02-07 14:33:47 +0800

    feat: Add font and global css with styled-component.

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -1,16 +1,51 @@
 import Link from 'next/link';
+import styled from 'styled-components';
 import Nav from './Nav';
 
+const StyledLogo = styled.h1`
+    font-size: 4rem;
+    margin-left: 2rem;
+    position: relative;
+    z-index: 2;
+    background: red;
+    transform: skew(-7deg);
+
+    a {
+        color: white;
+        text-decoration: none;
+        text-transform: uppercase;
+        padding: 0.5rem 1rem;
+    }
+`
+
+const StyledHeader = styled.header`
+    .bar {
+        display: grid;
+        grid-template-columns: auto 1fr;
+        justify-content: space-between;
+        align-items: center;
+        border-bottom: 10px solid var(--black, black);
+    }
+
+    .sub-bar {
+        display: grid;
+        grid-template-columns: auto 1fr;
+        border-bottom: 1px solid var(--black, black);
+    }
+`
+
 const Header = () => (
-    <header>
+    <StyledHeader>
         <div className="bar">
-            <Link href="/">Silk fit</Link>
+            <StyledLogo>
+                <Link href="/">Sick fits</Link>
+            </StyledLogo>
         </div>
         <div className="sub-bar">
             <p>Search</p>
         </div>
         <Nav />
-    </header>
+    </StyledHeader>
 )
 
 export default Header;
