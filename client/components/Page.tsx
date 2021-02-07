7f0127ae9c1398eabf4d327371a220fc171a10b3
趙子榮 <alan@25sprout.com>
2021-02-07 10:08:17 +0800

    feat: Use next/link to implement navigation.

--- client/components/Page.tsx
+++ client/components/Page.tsx
@@ -1,3 +1,5 @@
+import Header from './Header';
+
 // Reference: https://www.carlrippon.com/react-children-with-typescript/
 type Props = {
     children?: React.ReactNode;
@@ -5,6 +7,7 @@ type Props = {
 
 const Page: React.FC<Props> = ({ children }) => (
     <div>
+        <Header />
         <h2>I'm the page components</h2>
         {children}
     </div>
